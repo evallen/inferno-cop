54b747bb2ccdc59c988cef3894ae989207e0aec2
Evan Allen <evanallen@vt.edu>
2021-05-17 16:46:29 -0400

    updated for clayton best bit

--- inferno_cop.py
+++ inferno_cop.py
@@ -1,8 +1,6 @@
 import os
 import discord
-from message_checker import MessageChecker
 from config_loader import load_config
-from punish import punish
 
 
 class InfernoCop(discord.Client):
@@ -12,18 +10,14 @@ class InfernoCop(discord.Client):
 
     def __init__(self, config, **options):
         super().__init__(**options)
-        self.checker = MessageChecker(config)
 
     async def on_ready(self):
         print(f"We have logged in as {client.user}")
 
-    async def on_message(self, message):
-        if message.author == client.user:
-            return
+    async def on_message(self, message: discord.message):
+        if message.author.name == "claytonsav":
+            await message.channel.send(f"This really is clayton's best bit")
 
-        rule_broken = self.checker.validate_message(message.content)
-        if rule_broken is not None:
-            await punish(message, rule_broken)
 
 
 if __name__ == "__main__":
