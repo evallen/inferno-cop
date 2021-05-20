bfa6336b8fd8f265e43702565028f908654fa31a
Evan Allen <evanallen@vt.edu>
2021-05-19 20:22:00 -0400

    made it worse

--- inferno_cop.py
+++ inferno_cop.py
@@ -1,5 +1,6 @@
 import os
 import discord
+import random
 from config_loader import load_config
 
 
@@ -10,14 +11,24 @@ class InfernoCop(discord.Client):
 
     def __init__(self, config, **options):
         super().__init__(**options)
+        self.config = config
+        self.counter = 0
 
     async def on_ready(self):
         print(f"We have logged in as {client.user}")
 
     async def on_message(self, message: discord.message):
-        if message.author.name == "claytonsav":
-            await message.channel.send(f"This really is clayton's best bit")
+        if random.random() < self.config["special_chance"]:
+            await message.channel.send("**CONGRATULATIONS YOU WON THE SECRET PRIZE YAY 1/1000 CHANCE**")
+            for i in range(20):
+                await message.channel.send(f"https://tenor.com/view/when-the-drip-is-sus-gif-19616035")
 
+        if message.author.name == "claytonsav":
+            self.counter += 1
+
+            if self.counter == self.config["frequency"]:
+                self.counter = 0
+                await message.channel.send(f"This really is clayton's best bit")
 
 
 if __name__ == "__main__":
