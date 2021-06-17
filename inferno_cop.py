83113c55955f583152c6420c5c010e998a176e81
johnsnoa <johnson.noah.b@gmail.com>
2021-06-17 00:40:28 -0500

    Added some fun stuff
    Added something if patrick makes 10 messages. Also made it so that 1/10000 doesn't trigger on Inferno Cop messages

--- inferno_cop.py
+++ inferno_cop.py
@@ -12,23 +12,32 @@ class InfernoCop(discord.Client):
     def __init__(self, config, **options):
         super().__init__(**options)
         self.config = config
-        self.counter = 0
+        self.counter_clayton = 0
+        self.counter_patrick = 0
 
     async def on_ready(self):
         print(f"We have logged in as {client.user}")
 
     async def on_message(self, message: discord.message):
-        if random.random() < self.config["special_chance"]:
+        if (message.author.name != "Inferno Cop") and (random.random() < self.config["special_chance"]):
             await message.channel.send("**CONGRATULATIONS YOU WON THE SECRET PRIZE YAY 1/1000 CHANCE**")
             for i in range(20):
                 await message.channel.send(f"https://tenor.com/view/when-the-drip-is-sus-gif-19616035")
 
         if message.author.name == "claytonsav":
-            self.counter += 1
+            self.counter_clayton += 1
 
-            if self.counter == self.config["frequency"]:
-                self.counter = 0
+            if self.counter_clayton == self.config["frequency"]:
+                self.counter_clayton = 0
                 await message.channel.send(f"This really is clayton's best bit")
+
+        elif message.author.name == "Hekc":
+            self.counter_patrick += 1
+
+            if self.counter_patrick == self.config["frequency"]:
+                self.counter_patrick = 0
+                await message.react('847670479116173363')
+                await message.react('847670877063217182')
 
 
 if __name__ == "__main__":
