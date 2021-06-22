e9a5ecdd58c7ccbf06b2df0d7990be603d7eea40
johnsnoa <johnson.noah.b@gmail.com>
2021-06-22 00:54:36 -0500

    Added Ceganpost fcn
    Added Ceganpost fcn, doubled msg count to trigger Peet, added experimental thing to try and deal with username changes for cegan

--- inferno_cop.py
+++ inferno_cop.py
@@ -12,11 +12,18 @@ class InfernoCop(discord.Client):
     def __init__(self, config, **options):
         super().__init__(**options)
         self.config = config
+        self.cegan_username = "Praise the Morning Lord"
         self.counter_clayton = 0
         self.counter_patrick = 0
+        selt.counter_cegan = 0
 
     async def on_ready(self):
         print(f"We have logged in as {client.user}")
+
+    async def on_user_update(before, after):
+        if before.name == self.cegan_username:
+            print(f"Updating Cegan's username. Was: {before.name}. Now: {after.name}")
+            self.cegan_username = after.name
 
     async def on_message(self, message: discord.message):
         if (message.author.name != "Inferno Cop") and (random.random() < self.config["special_chance"]):
@@ -36,12 +43,20 @@ class InfernoCop(discord.Client):
             print(f"Patrick has sent {self.counter_patrick} messages.")
 
 
-            if self.counter_patrick == self.config["frequency"]:
+            if self.counter_patrick == (2*self.config["frequency"]):
                 print(f"Resetting counter to zero, as Patrick has sent {self.counter_patrick} messages.")
                 self.counter_patrick = 0
                 await message.add_reaction(client.get_emoji(847670479116173363))
                 await message.add_reaction(client.get_emoji(847670877063217182))
 
+        elif message.author.name == self.cegan_username:
+            self.counter_cegan += 1
+            print(f"Cegan has sent {self.counter_cegan} messages.")
+
+            if self.counter_cegan == self.config["frequency"]:
+                print(f"Resetting counter to zero, as Cegan has sent {self.counter_cegan} messages.")
+                self.counter_cegan = 0
+                await message.channel.send("**THIS IS A CERTIFIED CEGANPOST™**")
 
 if __name__ == "__main__":
     config_file_path = "config.yaml"
